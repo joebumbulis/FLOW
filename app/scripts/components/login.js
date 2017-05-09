a646beb160afe9818c19d2113e621bb621183f44
Joe Bumbulis <joebumbulis@gmail.com>
2017-05-09 17:23:18 -0500

    refs for login page

--- app/scripts/components/login.js
+++ app/scripts/components/login.js
@@ -1,36 +1,59 @@
 import React from "react";
 import { connect } from "react-redux";
 import container from "../containers/all.js";
+import { Route, Link, NavLink } from "react-router-dom";
 import { Navbar, Icon, Button, Input } from "react-materialize";
 
 class LogIn extends React.Component {
   constructor(props) {
     super(props);
+    this.logIn = this.logIn.bind(this);
+  }
+
+  goBack({ history }) {
+    return (
+      <button className="login-back" onClick={() => history.push("/open")}>
+        Back<Icon className="arrow-icon">keyboard_arrow_left</Icon>
+      </button>
+    );
+  }
+
+  logIn(e) {
+    e.preventDefault();
+    let email = e.target[0].value;
+    let password = e.target[1].value;
+    let emailsss = this.refs.emailsss.value;
+    console.log(email, password);
   }
 
   render() {
     return (
       <div>
         <nav className="login-navbar">
-          <button className="login-back">
-            Back<Icon className="arrow-icon">keyboard_arrow_left</Icon>
-          </button>
+          <Route render={this.goBack} />
           <h4 className="login-title">Log In</h4>
-          <button className="login-btn">Log In</button>
         </nav>
-        <form>
+        <form onSubmit={this.logIn}>
           <div className="login-inputs input-field">
-            <Input href="email" type="email" placeholder="email">
+            <Input ref="email" type="email" placeholder="email" validate>
               <Icon>email</Icon>
             </Input>
+            <i className="material-icons">email</i>
+            <input
+              ref="emailsss"
+              type="email"
+              placeholder="emailsss"
+              validate
+            />
             <Input
               className="pswd-input"
-              href="password"
+              ref="password"
               type="password"
               placeholder="password"
             >
               <Icon>lock_outline</Icon>
             </Input>
+            <button className="login-btn submit-login">Log In</button>
           </div>
         </form>
       </div>
